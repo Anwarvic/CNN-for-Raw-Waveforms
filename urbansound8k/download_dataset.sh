654a83ca8f650ba7be3a8c12171b376c0209b220
anwarvic <mohamedanwarvic@gmail.com>
2021-08-20 02:02:11 +0000

    created folder for urbansound8k dataset

--- urbansound8k/download_dataset.sh
+++ urbansound8k/download_dataset.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 echo "Downloading Urban Sound 8K (6GB)
-mkdir -p "urbansound8k"
-cd urbansound8k
-
 kaggle datasets download -d chrisfilo/urbansound8k
 
 echo "Unzipping.."
 unzip urbansound8k.zip
 
 echo "Done!"
-
-
